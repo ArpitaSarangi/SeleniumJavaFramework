16d6702b64e074e22f7575c6833e93f7defd9a95
Arpita Sarangi <arpitasarangi97@gmail.com>
2023-12-17 00:03:08 +0530

    Revising Selenium concepts

--- SeleniumJavaFramwork/src/Assignements/Assignment2.java
+++ SeleniumJavaFramwork/src/Assignements/Assignment2.java
@@ -1,10 +1,33 @@
 package Assignements;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class Assignment2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver", "D://SOFTWARES/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		driver.findElement(By.cssSelector("form-comp div.container form.ng-untouched.ng-pristine.ng-invalid div.form-group input.form-control.ng-untouched.ng-pristine.ng-invalid:nth-child(2)")).sendKeys("Arpita");
+		//driver.findElement(By.cssSelector("form-comp div.container form.ng-untouched.ng-pristine.ng-invalid div.form-group:nth-child(2) input.form-control.ng-untouched.ng-pristine.ng-invalid:nth-child(2)")).sendKeys("arpita123@gmail.com");
+		driver.findElement(By.name("email")).sendKeys("arita123@gmail.com");
+		driver.findElement(By.cssSelector("input[type='password']")).sendKeys("123456");
+		driver.findElement(By.id("exampleCheck1")).click();
+		
+		Select dropdown=new Select(driver.findElement(By.id("exampleFormControlSelect1")));
+		dropdown.selectByVisibleText("Female");
+		
+		driver.findElement(By.id("inlineRadio2")).click();
+		driver.findElement(By.name("bday")).sendKeys("28/04/1997");
+		driver.findElement(By.className("btn-success")).click();
+		
+		
+		System.out.println(driver.findElement(By.cssSelector(".alert.alert-success.alert-dismissible")).getText());
 	}
 
 }
---
+++ SeleniumJavaFramwork/src/Introduction/AlertsInSelenium.java
@@ -0,0 +1,36 @@
+package Introduction;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertsInSelenium {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "D://SOFTWARES/chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+		String name="Arpita";
+
+		driver.findElement(By.id("name")).sendKeys(name);
+
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input[id='alertbtn']")).click();
+		//driver.findElement(By.cssSelector("[id='alertbtn']")).click();
+
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+
+		driver.findElement(By.cssSelector("[id='confirmbtn']")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+
+
+	}
+
+}
