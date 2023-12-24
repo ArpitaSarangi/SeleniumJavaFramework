d6aa7931de32b8f3f04241a5c02a5b7e9718b635
Arpita Sarangi <arpitasarangi97@gmail.com>
2023-12-24 23:35:05 +0530

    screenshot

--- SeleniumJavaFramwork/src/Introduction/Miscelleanous.java
+++ SeleniumJavaFramwork/src/Introduction/Miscelleanous.java
@@ -1,11 +1,18 @@
 package Introduction;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.google.common.io.Files;
+
 public class Miscelleanous {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.setProperty("webdriver.chrome.driver", "D://SOFTWARES/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
@@ -18,9 +25,10 @@ public class Miscelleanous {
 		//Click on any link
 		//Login Page -verify login url
 		
-		
-		
 		driver.get("http://google.com");
+		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		//FileUtils.copyFile(src, new File("D:screenshot2.png"));
+		Files.copy(src, new File("D://screenshot5.png"));
 
 	}
 
