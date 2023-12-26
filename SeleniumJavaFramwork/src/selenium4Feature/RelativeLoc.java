665cb71517c5472e14fbcce12d778b3ab814417d
Arpita Sarangi <arpitasarangi97@gmail.com>
2023-12-26 23:12:40 +0530

    Selenium 4 Features

---
+++ SeleniumJavaFramwork/src/selenium4Feature/RelativeLoc.java
@@ -0,0 +1,31 @@
+package selenium4Feature;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
+
+public class RelativeLoc {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "D://SOFTWARES/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
+		System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText());
+
+		WebElement dateOfBirth = driver.findElement(By.cssSelector("[for='dateofBirth']"));
+		driver.findElement(with(By.tagName("input")).below(dateOfBirth)).click();
+
+		WebElement iceCreamCheckBox = driver
+				.findElement(By.xpath("//label[text()='Check me out if you Love IceCreams!']"));
+		driver.findElement(with(By.tagName("input")).toLeftOf(iceCreamCheckBox)).click();
+		
+	WebElement firstRadioButton=	driver.findElement(By.cssSelector("[id='inlineRadio1']"));
+	System.out.println(driver.findElement(with(By.tagName("label")).toRightOf(firstRadioButton)).getText());
+	}
+
+}
