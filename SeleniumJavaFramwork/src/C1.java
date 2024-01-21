0054f8a96537a24b4f66173de9cc14d38a7c028d
Arpita Sarangi <arpitasarangi97@gmail.com>
2024-01-21 20:21:51 +0530

    code changes

--- SeleniumJavaFramwork/src/C1.java
+++ SeleniumJavaFramwork/src/C1.java
@@ -1,12 +1,36 @@
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class C1 {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "D://SOFTWARES/chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
-
+	//stem.setProperty("webdriver.chrome.driver", "D://SOFTWARES/chromedriver.exe");
+		//bDriver driver=new ChromeDriver();
+		
+		String s="000110011001011";
+		
+		int count=0;
+		
+		for(int i=0;i<s.length();i++) {
+			if((i!=s.length()-1) && (s.contains("1").equals(s.charAt(i+1))
+				count++;	
+			}
+		else {
+			(==s.charAt(i))
+			}
+		}
+		
+		String m="";
+		for(int i=0;i<=count;i++) {
+		m=m+"0";
+		}
+		System.out.println(m);
+		
+			
+				
 	}
 
 }
--- SeleniumJavaFramwork/src/Introduction/AutoSuggestive.java
+++ SeleniumJavaFramwork/src/Introduction/AutoSuggestive.java
@@ -8,27 +8,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 public class AutoSuggestive {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "D://SOFTWARES/chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		driver.findElement(By.id("autosuggest")).sendKeys("ind");
 		Thread.sleep(3000);
-		List<WebElement> listAutoSuggestions=driver.findElements(By.xpath("//li[@class='ui-menu-item']/a"));
+		List<WebElement> listAutoSuggestions = driver.findElements(By.xpath("//li[@class='ui-menu-item']/a"));
 
-		for(WebElement options:listAutoSuggestions) {
-			if(options.getText().equalsIgnoreCase("India")){
+		for (WebElement options : listAutoSuggestions) {
+			if (options.getText().equalsIgnoreCase("India")) {
 				options.click();
 				break;
 			}
 		}
 		driver.close();
+
 	}
 
 }
